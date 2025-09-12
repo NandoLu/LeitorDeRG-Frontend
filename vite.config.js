deba0b2462dd3f5a3f19d816b32ba4d7436afc2e
Luiz <nandoluizprimeiro@gmail.com>
2025-09-12 09:08:13 -0300

    Refatorando e corrigindo utilização do tailwdind

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-/** @type {import('tailwindcss').Config} */
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+})
