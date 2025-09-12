288916b1050094e74da4595a9555a78f8b8b925b
Luiz <nandoluizprimeiro@gmail.com>
2025-09-12 09:30:33 -0300

    Mudando orientação

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import Header from "./components/Header.jsx"
-import Card from "./components/Card.jsx"
-import Upload from "./components/Upload.jsx"
-import BoxUpload from "./components/BoxUpload.jsx"
+import Header from "./components/Header.jsx";
+import Card from "./components/Card.jsx";
+import Upload from "./components/Upload.jsx";
+import BoxUpload from "./components/BoxUpload.jsx";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
       <Header />
-      <main className="p-6 grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+      <main className="p-6 flex flex-col items-center gap-6">
         {/* Exemplos de cards reutilizáveis */}
         <BoxUpload>
-
-        <Upload/>
+          <Upload />
         </BoxUpload>
         <Card name="Nome da Pessoa" document="123.123.123-12" />
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
